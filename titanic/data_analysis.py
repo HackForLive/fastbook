9d9fdcfd0dfcae955ea532c39a221b09100f0b14
HackForLive <michael.hrabe@gmail.com>
2026-03-14 20:13:15 +0000

    titanic

--- titanic/data_analysis.py
+++ titanic/data_analysis.py
@@ -5,7 +5,7 @@ import logging
 train_data = pd.read_csv(Path(__file__).parent / 'train.csv')
 
 
-logging.info(train_data.describe())
+logging.info(train_data.describe(include='all'))
 logging.info(train_data.info())
 
 # key observation is not to drop all missing data, but to exclude Cabin -> or restrict
